0c523e268261842aaf9d0a64d6f43f01cac8d59b
RDiGregorio <robertdigreg@gmail.com>
2023-10-06 14:51:31 -0400

    create a skeleton test for the customer controller

--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class Api::V1::CustomersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "customer can be created" do
+    assert !Customer.find_by(first_name: 'Alice', last_name: 'Liddell')
+    Customer.create(first_name: 'Alice', last_name: 'Liddell', license_number: '9KDB90', phone_number: '774-867-5309', email: 'alice.liddell@gmail.com')
+    assert Customer.find_by(first_name: 'Alice', last_name: 'Liddell').license_number == '9KDB90'
+  end
 end

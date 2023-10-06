a5806bc153c9458f2cba21b5e68785c641c9548f
RDiGregorio <robertdigreg@gmail.com>
2023-10-06 13:27:00 -0400

    add skeleton controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :customers do
+        resources :vehicles do
+          resources :reservations
+        end
+      end
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

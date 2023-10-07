fe9986b644342a7e62a5c3b67765c01226af4367
RDiGregorio <robertdigreg@gmail.com>
2023-10-07 18:15:44 -0400

    tests and bug fixes

--- test/controllers/api/v1/scheduling_controller_test.rb
+++ test/controllers/api/v1/scheduling_controller_test.rb
@@ -28,6 +28,62 @@ class Api::V1::SchedulingControllerTest < ActionDispatch::IntegrationTest
     assert_equal json_response["status"], "Pending"
   end
 
+  test "making multiple reservations does not duplicate customers or vehicles" do
+    # We are making 2 appointments, to fix 2 different things, on the same car, at different dates.
+
+    post "http://127.0.0.1:3000/api/v1/scheduling", params: {
+      "first_name": "Alice",
+      "last_name": "Liddell",
+      "license_number": "S54318719",
+      phone_number: '774-867-5309',
+      email: 'alice.liddell@gmail.com',
+      "registration_number": "9KDB90",
+      "make": "Nissan",
+      "model": "Sentra",
+      "color": "White",
+      "date": "2023-10-06",
+      "time": "10:30:00",
+      "description": "broken engine",
+      "status": "Pending"
+    }
+
+    post "http://127.0.0.1:3000/api/v1/scheduling", params: {
+      "first_name": "Alice",
+      "last_name": "Liddell",
+      "license_number": "S54318719",
+      phone_number: '774-867-5309',
+      email: 'alice.liddell@gmail.com',
+      "registration_number": "9KDB90",
+      "make": "Nissan",
+      "model": "Sentra",
+      "color": "White",
+      "date": "2023-10-07",
+      "time": "10:30:00",
+      "description": "broken window",
+      "status": "Pending"
+    }
+
+    # Verify 2 reservations are made.
+
+    get "http://127.0.0.1:3000/api/v1/scheduling", params: { "license_number": "S54318719" }
+    assert_equal json_response.size, 2
+    assert_equal json_response[0]["description"], "broken engine"
+    assert_equal json_response[1]["description"], "broken window"
+
+    # Verify only 1 customer is made.
+
+    get "http://127.0.0.1:3000/api/v1/customers", params: { "license_number": "S54318719" }
+    assert_equal json_response[0]["license_number"], "S54318719"
+    assert_equal json_response.size, 1
+
+    # Verify only 1 vehicle is made.
+
+    id = json_response[0]["id"]
+    get "http://127.0.0.1:3000/api/v1/customers/#{id}/vehicles", params: { "registration_number": "9KDB90" }
+    assert_equal json_response[0]["registration_number"], "9KDB90"
+    assert_equal json_response.size, 1
+  end
+
   test "looking up reservations for a customer by license number" do
     post "http://127.0.0.1:3000/api/v1/scheduling", params: {
       "first_name": "Alice",
@@ -87,8 +143,8 @@ class Api::V1::SchedulingControllerTest < ActionDispatch::IntegrationTest
       "status": "Pending"
     }
 
-    # A customer with the same name, but a completed job. The license number and registration number are different. The
-    # have the same name, phone number, email because they are mother and daughter and decided to share accounts.
+    # A customer with the same name, but a completed job. The license number and registration number are different. They
+    # have the same name, phone number, and email because they are mother and daughter and decided to share accounts.
 
     post "http://127.0.0.1:3000/api/v1/scheduling", params: {
       "first_name": "Alice",
@@ -111,5 +167,47 @@ class Api::V1::SchedulingControllerTest < ActionDispatch::IntegrationTest
     assert_equal json_response[0]["description"], "broken engine"
     assert_equal json_response[0]["status"], "Pending"
     assert_equal json_response[0]["date"], "2023-10-06"
+  end
+
+  test "looking up reservations for a customer by name fails if multiple people have the same name and open reservations" do
+    post "http://127.0.0.1:3000/api/v1/scheduling", params: {
+      "first_name": "Alice",
+      "last_name": "Liddell",
+      "license_number": "S54318719",
+      phone_number: '774-867-5309',
+      email: 'alice.liddell@gmail.com',
+      "registration_number": "9KDB90",
+      "make": "Nissan",
+      "model": "Sentra",
+      "color": "White",
+      "date": "2023-10-06",
+      "time": "10:30:00",
+      "description": "broken engine",
+      "status": "Pending"
+    }
+
+    # A customer with the same name. The license number and registration number are different. They have the same name,
+    # phone number, and email because they are mother and daughter and decided to share accounts.
+
+    post "http://127.0.0.1:3000/api/v1/scheduling", params: {
+      "first_name": "Alice",
+      "last_name": "Liddell",
+      "license_number": "S54318720",
+      phone_number: '774-867-5309',
+      email: 'alice.liddell@gmail.com',
+      "registration_number": "9KDB91",
+      "make": "Nissan",
+      "model": "Sentra",
+      "color": "White",
+      "date": "2023-10-06",
+      "time": "10:30:00",
+      "description": "broken engine",
+      "status": "Pending"
+    }
+
+    # A reason for failure and a possible resolution is given.
+
+    get "http://127.0.0.1:3000/api/v1/scheduling", params: { "first_name": "Alice", "last_name": "Liddell" }
+    assert_equal json_response["error"], "Found multiple customers. Please search by license number."
   end
 end
